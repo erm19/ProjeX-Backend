dff8a62840884a0dba57699ab859679ce2ebd613
erm19 <eyal.moskowitz@gmail.com>
2024-11-16 21:44:58 +0200

    ignore async handler for now

--- api-gateway-service/src/routes/api/auth/handlers.ts
+++ api-gateway-service/src/routes/api/auth/handlers.ts
@@ -1,15 +1,24 @@
 import axios from "axios";
-import { Request, Response, NextFunction } from "express";
 import { asyncHandler } from "../../../utils";
+import { NextFunction, Request, Response } from "express";
 
-export const signupHandler = asyncHandler(async (req, res, next) => {
-  const response = await axios.post("http://localhost:3001/signup", req.body);
-});
+export const signupHandler =
+  // asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const response = await axios.post("http://localhost:3001/signup", req.body);
+  };
+// );
 
-export const loginHandler = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  await axios.get("http://localhost:3001/login");
-});
+export const loginHandler =
+  //  asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    await axios.get("http://localhost:3001/login");
+  };
+// );
 
-export const logoutHandler = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  await axios.get("http://localhost:3001/logout");
-});
+export const logoutHandler =
+  //  asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    await axios.get("http://localhost:3001/logout");
+  };
+// );
--- authentication-service/src/main.ts
+++ authentication-service/src/main.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { authRouter } from "./routes";
 
 async function startService() {
   const app = express();
   app.use(express.json());
   app.use(cors());
 
-  // app.use("/", appRouter);
+  app.use("/", authRouter);
 
   app.listen(3001, () => {
     console.log("Server running on port 3001");
