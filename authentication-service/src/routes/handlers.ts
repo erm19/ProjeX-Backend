bae92f6cc2d4c5f9eae578653d312f44e18f5846
erm19 <eyal.moskowitz@gmail.com>
2024-11-19 21:50:42 +0200

    chore: api setup

--- authentication-service/src/routes/handlers.ts
+++ authentication-service/src/routes/handlers.ts
@@ -1,9 +1,25 @@
 import expressAsyncHandler from "express-async-handler";
 
 export const signupHandler = expressAsyncHandler(async (req, res) => {
-  // res.send("abc");
+  const email = req.body.email;
+  const password = req.body.password;
+  const role = req.body.role;
+  const companyName = req.body.companyName;
+  const companyId = req.body.companyId;
+  const companyRole = req.body.companyRole;
+  const officeName = req.body.officeName;
+  const licenceNum = req.body.licenceNum;
+
+  res.send(true);
 });
 
 export const loginHandler = expressAsyncHandler(async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
   res.send("abc");
+});
+
+export const logoutHandler = expressAsyncHandler(async (req, res) => {
+  res.send(true);
 });
