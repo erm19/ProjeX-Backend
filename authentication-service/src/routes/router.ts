2146f739ea1a4a623084ce3ad42ca195dbbeed04
erm19 <eyal.moskowitz@gmail.com>
2024-11-28 12:58:50 +0200

    chore: add verify route to auth

--- authentication-service/src/routes/router.ts
+++ authentication-service/src/routes/router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginHandler, logoutHandler, refreshHandler, signupHandler } from "./handlers";
+import { loginHandler, logoutHandler, refreshHandler, signupHandler, verifyHandler } from "./handlers";
 import { validateTokenGuard } from "./token.guard";
 
 export const authRouter = Router();
@@ -12,6 +12,8 @@ authRouter.post("/signup", signupHandler);
 
 authRouter.post("/login", loginHandler);
 
-authRouter.post("/logout", validateTokenGuard, logoutHandler);
+authRouter.get("/logout", validateTokenGuard, logoutHandler);
 
 authRouter.post("/refresh", validateTokenGuard, refreshHandler);
+
+authRouter.post("/verify", verifyHandler);
