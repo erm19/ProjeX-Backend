91972c0f56d8d1ec2b537320b8413bde151e7294
erm19 <eyal.moskowitz@gmail.com>
2025-01-14 23:56:41 +0200

    chore: export converter

--- @urbanix/error-handling/src/index.ts
+++ @urbanix/error-handling/src/index.ts
@@ -4,4 +4,4 @@ export * from "./errors/base.error";
 export * from "./errors/internal-server.error";
 export * from "./errors/not-found.error";
 export * from "./errors/validation.error";
-export { errorHandler } from "./middleware/error";
+export { errorHandler, convertUnknownToError } from "./middleware/error";
