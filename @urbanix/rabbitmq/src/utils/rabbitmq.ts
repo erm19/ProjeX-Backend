eecc016e9eb6d5bd9005e9c86f597cf5747c1b6e
erm19 <eyal.moskowitz@gmail.com>
2025-01-30 19:08:24 +0200

    fix(rabbitmq): update version to 1.1.2 and improve connection handling with enhanced type definitions

--- @urbanix/rabbitmq/src/utils/rabbitmq.ts
+++ @urbanix/rabbitmq/src/utils/rabbitmq.ts
@@ -1,14 +1,15 @@
 import { InternalServerError } from "@urbanix/error-handling";
-import { connect, Connection } from "amqplib";
+import { connect, Connection, Channel } from "amqplib";
 
 let connection: Connection | null = null;
 
 // Create or reuse a RabbitMQ connection
-export async function getRabbitMQConnection(url: string) {
+export async function getRabbitMQConnection(url: string): Promise<Connection> {
   try {
     if (!connection) {
       connection = await connect(url); // Replace with actual RabbitMQ URL
     }
+    console.log(`RabbitMQ connection established at ${url}`);
     return connection;
   } catch (error) {
     throw new InternalServerError("Failed to create RabbitMQ connection");
@@ -16,10 +17,10 @@ export async function getRabbitMQConnection(url: string) {
 }
 
 // Create a channel from the connection
-export async function createRabbitMQChannel(url: string) {
+export async function createRabbitMQChannel(url: string): Promise<Channel> {
   try {
     const connection = await getRabbitMQConnection(url);
-    return connection.createChannel();
+    return await connection.createChannel();
   } catch (error) {
     throw new InternalServerError("Failed to create RabbitMQ channel");
   }
