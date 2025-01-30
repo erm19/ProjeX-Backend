2bdafb9906391a8d65845565a7aa5d7d8b393f6c
erm19 <eyal.moskowitz@gmail.com>
2025-01-30 20:38:31 +0200

    feat(user-repository): add create, update, and delete methods to IUserRepository and MongoUserRepository

--- ent-offers-service/src/infrastructure/database/mongo-user.repository.ts
+++ ent-offers-service/src/infrastructure/database/mongo-user.repository.ts
@@ -1,3 +1,4 @@
+import { Schema } from "mongoose";
 import { IUser } from "../../domain/entities";
 import { IUserRepository } from "../../domain/repositories";
 import { User } from "../orm";
@@ -5,5 +6,17 @@ import { User } from "../orm";
 export class MongoUserRepository implements IUserRepository {
   async findByEmail(email: string): Promise<IUser | null> {
     return await User.findOne({ email: email });
+  }
+
+  async create(id: string, email: string): Promise<IUser> {
+    return await User.create({ id, email });
+  }
+
+  async update(id: string, offers: Schema.Types.ObjectId[]): Promise<IUser | null> {
+    return await User.findOneAndUpdate({ id }, { entOffers: offers });
+  }
+
+  async delete(id: string): Promise<void> {
+    await User.deleteOne({ id });
   }
 }
