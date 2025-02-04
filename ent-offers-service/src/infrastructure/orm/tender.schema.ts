6cbc4565a7091797ab52a6755489b313d87190af
erm19 <eyal.moskowitz@gmail.com>
2025-02-04 20:40:03 +0200

    feat(tender): add collection name to TenderSchema for better organization

--- ent-offers-service/src/infrastructure/orm/tender.schema.ts
+++ ent-offers-service/src/infrastructure/orm/tender.schema.ts
@@ -1,8 +1,11 @@
 import { model, Schema } from "mongoose";
 import { ITender } from "../../domain/entities";
 
-const TenderSchema = new Schema<ITender>({
-  title: { type: String },
-});
+const TenderSchema = new Schema<ITender>(
+  {
+    title: { type: String },
+  },
+  { collection: "tenders" }
+);
 
 export const EntTender = model<ITender>("EntTender", TenderSchema);
